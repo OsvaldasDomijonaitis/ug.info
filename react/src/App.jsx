bb0587fda6422bdc867cff701d543d9353f548eb
Gerardas Kavaliauskas <gerardas.kavaliauskas@stud.ktmc.lt>
2025-03-12 12:02:13 +0200

    Pridetas Dizainas

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,29 +1,13 @@
-import { useEffect, useState } from "react";
-
-import Dashboard from "./Dashboard";
-import Login from "./Login";
-
-import TokenContext from "./TokenContext";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Dashboard, Auth } from "@/layouts";
 
 function App() {
-  // const [token, setToken] = useState("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyaWQiOjEyLCJpYXQiOjE3MzQ2OTM4ODgsImV4cCI6MTczNDY5NzQ4OH0.RmOekBrw76fhaHcaL4cweNQrTmkXNjg3YYcSLWw-9U8");
-  const [token, setToken] = useState("");
-
-  // sesijos užkrovimas iš sessionStorrage
-  const loadSessionData = () => {
-    if (sessionStorage.getItem("token")) {
-      setToken(sessionStorage.getItem("token"));
-    }
-  };
-
-  useEffect(() => {
-    loadSessionData();
-  }, []);
-
   return (
-    <TokenContext.Provider value={[token, setToken]}>
-      {token ? <Dashboard /> : <Login />}
-    </TokenContext.Provider>
+    <Routes>
+      <Route path="/dashboard/*" element={<Dashboard />} />
+      <Route path="/auth/*" element={<Auth />} />
+      <Route path="*" element={<Navigate to="/dashboard/home" replace />} />
+    </Routes>
   );
 }
 
--- react/vite.config.js
+++ react/vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/",
   plugins: [react()],
+  resolve: {
+    alias: [{ find: "@", replacement: "/src" }],
+     },
   server: {
     host: true,
     port: 80,
@@ -12,5 +13,4 @@ export default defineConfig({
       usePolling: true
     }
   }
-})
-
+});
